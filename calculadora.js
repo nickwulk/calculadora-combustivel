452849b2ecfff1905f4c9a376a7783c97cabe7da
Nick <nicolas.wulk@gmail.com>
2022-11-19 13:43:54 -0300

    Corrigindo app para aceitar vírgula

--- calculadora.js
+++ calculadora.js
@@ -4,8 +4,26 @@ var kmA = document.querySelector("#inputKmAlcool")
 var pG = document.querySelector("#inputPrecoGasolina")
 var pA = document.querySelector("#inputPrecoAlcool")
 
+function substVirgula(){
+    
+    if (kmG.value.includes(",") === true){
+        kmG.value = kmG.value.replace(",", ".")
+    }
+    if (kmA.value.includes(",") === true){
+      kmA.value = kmA.value.replace(",", ".")
+    }
+    if (pA.value.includes(",") === true){
+      pA.value = pA.value.replace(",", ".")
+    }
+    if (pG.value.includes(",") === true){
+      pG.value = pG.value.replace(",", ".")
+    }
+}  
+
 function calc(){
     if (kmG.value !== '' && kmA.value !== '' && pA.value !== '' && pG.value !== ''){
+
+    substVirgula()
 
     var p = (pA.value/pG.value).toFixed(2)
     var r = (kmA.value/kmG.value).toFixed(2)
